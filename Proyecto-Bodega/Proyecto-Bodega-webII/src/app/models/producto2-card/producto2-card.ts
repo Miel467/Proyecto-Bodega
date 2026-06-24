6240678f0dbbfd89b2d47a9a02c7e215235538c7
Miel467 <dalianazurita5@gmail.com>
2026-06-24 00:00:52 -0300

    Add files via upload

---
+++ Proyecto-Bodega/Proyecto-Bodega-webII/src/app/models/producto2-card/producto2-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto2-card',
+  imports: [],
+  templateUrl: './producto2-card.html',
+  styleUrl: './producto2-card.css',
+})
+export class Producto2Card {}
