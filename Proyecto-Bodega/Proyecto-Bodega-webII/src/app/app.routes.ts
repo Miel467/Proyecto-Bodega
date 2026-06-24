9c2a1f0e25aba6853accd05fd3de10038b802cb0
Miel467 <dalianazurita5@gmail.com>
2026-06-24 16:14:54 -0300

    Delete Proyecto-Bodega directory

--- Proyecto-Bodega/Proyecto-Bodega-webII/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {Inicio} from './pages/inicio/inicio';
-
-
-export const routes: Routes = [       
-      { path: '', component: Inicio}
-        
-];
-    
--- Proyecto-Bodega/Proyecto-Bodega-webII/src/app/models/producto2-card/producto2-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-producto2-card',
-  imports: [],
-  templateUrl: './producto2-card.html',
-  styleUrl: './producto2-card.css',
-})
-export class Producto2Card {}
