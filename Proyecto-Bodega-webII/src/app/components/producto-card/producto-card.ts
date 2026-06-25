48fbabf2e16a727f000ad01b9e02c9c4b345a9d3
Miel467 <dalianazurita5@gmail.com>
2026-06-25 15:44:42 -0300

    Delete Proyecto-Bodega-webII directory

--- Proyecto-Bodega-webII/src/app/components/producto-card/producto-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-producto-card',
-  imports: [],
-  templateUrl: './producto-card.html',
-  styleUrl: './producto-card.css',
-})
-export class ProductoCard {}
