381918e479147094e48c30ae31230b26c57d25f1
Miel467 <dalianazurita5@gmail.com>
2026-06-30 16:06:05 -0300

    Delete Proyecto-Bodega-webII directory

--- Proyecto-Bodega-webII/src/app/pages/sucursales/sucursales.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-sucursales",
-  imports: [],
-  templateUrl: "./sucursales.html",
-  styleUrl: "./sucursales.css",
-})
-export class Sucursales {}
